8f68a7674f6ec7a416fe3ba0f1bf5167da6a6bf4
DESKTOP-CSSL40G\elwla <elwlahd555@naver.com>
2021-04-27 15:25:01 +0900

    feat : 📝 accountController 생성

---
+++ src/main/java/com/studyolle/repository/dto/Account.java
@@ -0,0 +1,92 @@
+package com.studyolle.repository.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+	// 회원 정보
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime emailCheckTokenGeneratedAt;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+    //알람 관련 정보
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb = true;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb = true;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb = true;
+
+//    @ManyToMany
+//    private Set<Tag> tags = new HashSet<>();
+//
+//    @ManyToMany
+//    private Set<Zone> zones = new HashSet<>();
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+        this.emailCheckTokenGeneratedAt = LocalDateTime.now();
+    }
+
+    public void completeSignUp() {
+        this.emailVerified = true;
+        this.joinedAt = LocalDateTime.now();
+    }
+
+    public boolean isValidToken(String token) {
+        return this.emailCheckToken.equals(token);
+    }
+
+    public boolean canSendConfirmEmail() {
+        return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
+    }
+
+}
