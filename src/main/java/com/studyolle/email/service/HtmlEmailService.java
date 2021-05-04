02414881ef42959ee31cc4090cf628f429972e1f
elwlahd555 <elwlahd555>
2021-05-04 23:12:31 +0900

    feat : 📝 로그인 기억하기

--- src/main/java/com/studyolle/email/service/HtmlEmailService.java
+++ src/main/java/com/studyolle/email/service/HtmlEmailService.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Profile({"local", "test"})
 @Component
 @RequiredArgsConstructor
 public class HtmlEmailService implements EmailService{
